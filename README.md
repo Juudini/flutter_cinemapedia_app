a101673e7d1d1263a59d64e8c6936d4e70afbbfb
Juudini <juudinidev@gmail.com>
2024-03-19 22:51:59 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# cinemapedia
+
+1. Copy `.env.template` and renamed to `.env`
+2. Copy with your keys
---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,13 @@
+import 'package:go_router/go_router.dart';
+import 'package:cinemapedia/presentation/screens/screens.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(),
+    ),
+  ],
+);
